df66061baeca9ee1cb3d384b8cc289c8c50e1c4e
Paul <paul@thelongs.ws>
2015-04-23 12:44:53 -0700

    HW2 complete

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # You're in the dangerzone
+This is my ECE 571 work from spring 2015
--- hw2/hdl/paramClock.sv
+++ hw2/hdl/paramClock.sv
@@ -1,4 +1,4 @@
-`timescale 1ns/1ps
+`timescale 1ns/1ps 
 // paramaterizable clock module
 module pClock(output bit tick);
   
